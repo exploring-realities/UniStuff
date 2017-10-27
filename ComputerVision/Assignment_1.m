25b013888e84f23f6c52b9d304bd3fd45f1dae98
pandafork <frau.gabel@gmail.com>
2017-10-27 14:26:43 +0200

    Computer Vision assignment 1

--- ComputerVision/Assignment_1.m
+++ ComputerVision/Assignment_1.m
@@ -3,4 +3,9 @@ M1 = [1 2; 3 4; 5 6];
 M1([2 3],:) = M1([3 2],:)
 
 %assignment 1.2
-Mrand = randi([0 10], 10, 10)
+Mrand = randi([0 10], 10, 10);
+Mrand([1],:) = 1
+Mrand(:,[1]) = 1
+
+%assignment 1.3
+row200 = [[8 6 4 2], zeros(1, 195), [10]]
